4147d76c7766ee63c8107e99aede449802f51286
Martin Homuth <martin.homuth@emlix.com>
2019-05-25 20:12:42 +0200

    adjust fontsize
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVu Sans Mono:bold:size=6:antialias=true:hinting=true";
+static char *font = "DejaVu Sans Mono:bold:size=10:antialias=true:hinting=true";
 static int borderpx = 2;
 
 /*
