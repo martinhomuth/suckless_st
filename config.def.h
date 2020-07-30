f373fc0f7d84307df1a1e6f5d466d32d07969b28
Martin Homuth <martin@followthestack.tech>
2020-07-30 12:47:00 +0200

    reduce font size
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Mono:bold:size=12:antialias=true:hinting=true";
+static char *font = "Mono:bold:size=8:antialias=true:hinting=true";
 static int borderpx = 2;
 
 /*
