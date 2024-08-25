28e9b2bd48d44448b644737092a2ebf0d8ab7eb0
Martin Homuth <martin@followthestack.tech>
2024-08-25 21:20:03 +0200

    color: black: fix value and remove comment
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- config.def.h
+++ config.def.h
@@ -96,7 +96,7 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
   /* 8 normal colors */
-  [0] = "#32302f", /* hard contrast: #1d2021 / soft contrast: #32302f */
+  [0] = "#282828", /* black   */
   [1] = "#cc241d", /* red     */
   [2] = "#98971a", /* green   */
   [3] = "#d79921", /* yellow  */
